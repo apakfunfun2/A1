51123d21b5a8627680d0aabe3d5dc8a2be87f1bc
apakfunfun2 <apak@funfun2.ca>
2019-07-16 21:56:33 -0400

    Add files via upload

---
+++ assets/data/myClasses.ts
@@ -0,0 +1,15 @@
+import { ClassInfo } from '../../app/classInfo';
+
+export const MYCLASSES: ClassInfo[] = [{
+    cname: 'SYST24444',
+    room: 'E2000',
+    dow: 'Tuesday',
+    tod: '3-6'
+},
+{
+    cname: 'SYST30050',
+    room: 'G3000',
+    dow: 'Friday',
+    tod: '8-11'
+}
+];
